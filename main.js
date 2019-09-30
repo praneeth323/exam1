6d18d9c269d805625dc3256cd6d10a8acfd93d62
praneeth323 <46974375+praneeth323@users.noreply.github.com>
2019-09-30 12:45:04 -0500

    updated Html and js

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ function calculateAverage()
 {
     const firstNumber=Number(document.getElementById("firstNumber").value)
     const secondNumber=Number(document.getElementById("secondNumber").value)
-    const thirdNumber=Number(document.getElementById("thirdNumber").value)
-    const averageOfNumbers=(firstNumber+secondNumber+thirdNumber)/3;
+
+    const averageOfNumbers=(firstNumber*secondNumber);
 
     document.getElementById("average").value = averageOfNumbers;
     
